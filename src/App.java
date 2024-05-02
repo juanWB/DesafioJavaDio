43f698a794d3b1fe6b8112f545883c5bba21318a
Juan Alejandro <juan.acv.06@gmail.com>
2024-05-02 17:04:36 -0300

    Desafio para por em práticas os fundamentos básicos.

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.print("Digite seu nome: ");
+        String nome = leitor.nextLine();
+
+        System.out.println(" ");
+        System.out.print("Digite sua idade: ");
+        int idade = leitor.nextInt();
+
+        System.out.println("Olá, me chamo " + nome + " e tenho " + idade + " anos.");
+       
+
+
+
+
+
+    }
+}
